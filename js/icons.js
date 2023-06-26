f02ad02816612dc6acb7794dba8811bf3919dbf1
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-06-27 00:11:34 +0530

    images

--- js/icons.js
+++ js/icons.js
@@ -3,16 +3,27 @@ function fetchRandomIcon() {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1'
-    }
+    },
   };
 
-  fetch('https://api.iconfinder.com/v4/iconsets?count=10', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-}
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // cors-anywhere proxy URL
+  const apiUrl = 'https://api.iconfinder.com/v4/iconsets?count=10'; // Iconfinder API URL
 
+  fetch(proxyUrl + apiUrl, options)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      // Process the response and retrieve the random icon
+      const iconsets = response.iconsets;
+      const randomIcon = iconsets[Math.floor(Math.random() * iconsets.length)];
+
+      console.log(randomIcon);
+      // Display the random icon
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
 
 // Call the fetchRandomIcon function to fetch and display a random icon
 fetchRandomIcon();
