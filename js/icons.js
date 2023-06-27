64ebb361dce641c61c56b53637b5aa334449577b
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-06-27 15:39:59 +0530

    images

--- js/icons.js
+++ js/icons.js
@@ -2,11 +2,11 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer aeQYzZuoK2vNv7UPLWFh8XFnUO4vmWsL4Uuzu5fdJaBlwKnM8y3Ahc36StviDNZA'
+    Authorization: 'Bearer aeQYzZuoK2vNv7UPLWFh8XFnUO4vmWsL4Uuzu5fdJaBlwKnM8y3Ahc36StviDNZA',
   }
 };
 
-fetch('https://api.iconfinder.com/v4/iconsets',options)
+fetch('http://api.iconfinder.com/v4/iconsets', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
